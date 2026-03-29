5072f45db73c0f8ce897ca1211e2ba60a0e51923
Julius Sommer <julius@hof-sommer.org>
2026-03-30 00:52:01 +0200

    Update save_figure function to accept Path objects and improve path handling

--- src/phd_ui/plotting/export.py
+++ src/phd_ui/plotting/export.py
@@ -1,5 +1,7 @@
 from matplotlib.figure import Figure
+from pathlib import Path
 
-def save_figure(fig: Figure, figure_path: str, name: str, dpi: int = 300) -> None:
-    fig.savefig(figure_path + rf"\{name}.png", dpi=dpi)
-    fig.savefig(figure_path + rf"\{name}.pdf", dpi=dpi)
+def save_figure(fig: Figure, figure_path: str | Path, name: str, dpi: int = 300) -> None:
+    figure_path = Path(figure_path)
+    fig.savefig(figure_path / f"{name}.png", dpi=dpi)
+    fig.savefig(figure_path / f"{name}.pdf", dpi=dpi)
