543313a87b5ceaf0460e728d61e0b2fd190977e5
Julius Sommer <julius@hof-sommer.org>
2026-05-06 22:42:34 +0200

    Add get_figsizes and get_figsize to __all__ in plotting module

--- src/phd_ui/plotting/__init__.py
+++ src/phd_ui/plotting/__init__.py
@@ -1,6 +1,7 @@
 from phd_ui.plotting.core import update_params, update_params_string, PARAMS, FIGSIZE, set_locators, fix_right_ylabels
 from phd_ui.plotting.conversion import INCH2CM, inches_to_cm, cm_to_inches
 from phd_ui.plotting.export import save_figure
+from phd_ui.plotting.figsize import get_figsizes, get_figsize
 
 
 __all__ = [
@@ -13,5 +14,7 @@ __all__ = [
     "cm_to_inches",
     "save_figure",
     "set_locators",
-    "fix_right_ylabels"
+    "fix_right_ylabels",
+    "get_figsizes",
+    "get_figsize"
 ]
